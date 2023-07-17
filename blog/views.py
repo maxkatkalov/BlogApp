539296523f1cf762948725119417d9a74d7d7249
Max Katkalov <makskatkalov@gmail.com>
2023-07-17 11:19:24 +0300

    CUD views added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -8,7 +9,24 @@ class BlogListView(ListView):
     template_name = "home.html"
 
 
-class BlogDetailView(DeleteView):
+class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
-    context_object_name = "details_of_post"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "body", "author"]
+
+
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
