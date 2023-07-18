111273d7abe795c46d1d5bf9ffdacf47da773e61
Max Katkalov <makskatkalov@gmail.com>
2023-07-18 10:44:17 +0000

    some changes

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = "/home2/techone1/blog.techone.pp.ua/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
